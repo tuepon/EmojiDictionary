f19bc0bf9ebb2da36ed4e042f3f6b06c2a99f848
Tomonori Ueno <t.uepon@gmail.com>
2020-12-11 11:49:15 +0700

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by SEZAX development on 12/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
